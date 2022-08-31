3fcff49c8b24d4b93ceee8eee29a0002c38e6c7f
Lucas Calabria <lucascalabria86@gmail.com>
2022-08-31 15:31:49 -0300

    Commit depois do Feedback

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+let nomeUsuario = prompt("Qual seu nome ?");
+let comida1 = prompt("Qual sua primeira comida favorita ?");
+let comida2 = prompt("Qual sua segunda comida favorita ?");
+let comida3 = prompt("Qual sua terceira comida favorita ?");
+
+console.log(`Estas são as comidas favoritas de: ${nomeUsuario} \n${comida1} \n${comida2} \n${comida3}`); 
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,15 @@
+// a) Remova o excesso de espaços no final da string;
+
+// b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços;
+
+// c) Substitua os traços `________` por “sticioso”.
+
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+let minhaTrim = minhaString.trim();   // Letra A
+
+console.log(minhaString.length);
+console.log(minhaTrim.length);      // Letra B
+
+let troca = minhaString.replaceAll("________", "“sticioso”");    //Letra C
+console.log(troca);
